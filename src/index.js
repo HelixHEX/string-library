ff43802a02135cb0117db11fbb0664ddeaaef70e
Elias <e.wambugu192@gmail.com>
2022-08-19 13:13:10 -0700

    finished kebobCase

--- src/index.js
+++ src/index.js
@@ -7,28 +7,44 @@ const allCaps = (str) => {
 };
 
 const capitalizeWords = (str) => {
-  return str.split(" ").map((word, index) => {
-    if (index !== 0) {
-      if (
-        word !== "the" &&
-        word !== "in" &&
-        word !== "a" &&
-        word !== "an" &&
-        word !== "and" &&
-        word !== "but" &&
-        word !== "for" &&
-        word !== "at" &&
-        word !== "but" &&
-        word !== "from"
-      ) {
+  return str
+    .split(" ")
+    .map((word, index) => {
+      if (index !== 0) {
+        if (
+          word !== "the" &&
+          word !== "in" &&
+          word !== "a" &&
+          word !== "an" &&
+          word !== "and" &&
+          word !== "but" &&
+          word !== "for" &&
+          word !== "at" &&
+          word !== "but" &&
+          word !== "from"
+        ) {
+          return capitalize(word);
+        } else return word;
+      } else {
         return capitalize(word);
-      } else return word;
-    } else {
-      return capitalize(word);
-    }
-  }).join(" ")
+      }
+    })
+    .join(" ");
 };
 
-const removeExtraSpaces = str => {
-  return str.trim().split(' ').filter(w => w).join(' ')
-}
+const removeExtraSpaces = (str) => {
+  return str
+    .trim()
+    .split(" ")
+    .filter((w) => w)
+    .join(" ");
+};
+
+const kebobCase = (str) => {
+  return removeExtraSpaces(str)
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[&\/\\#^+()$~%.'":*?<>{}!@]/g, "");
+};
+
